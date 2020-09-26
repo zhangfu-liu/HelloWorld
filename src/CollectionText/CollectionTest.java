31b78404e277da8c686446e7aae21b81513f37f7
zhangfu-liu <1816894130@qq.com>
2020-09-27 01:15:40 +0800

    123

--- src/CollectionText/CollectionTest.java
+++ src/CollectionText/CollectionTest.java
@@ -11,21 +11,25 @@ import java.util.Iterator;
  * @Since 2020-04-02 13:12
  */
 public class CollectionTest {
-    public static void main(String[]args){
-        Collection c = new ArrayList();
-        c.add(new person("张三",16));
-        c.add(new person("张六",18));
-        c.add(new person("张四",16));
-        c.add(new person("张五",18));
+
+    public static void main(String[] args) {
+
+        Collection c = new ArrayList ();
+
+
+        c.add (new person ("张三", 16));
+        c.add (new person ("张六", 18));
+        c.add (new person ("张四", 16));
+        c.add (new person ("张五", 18));
        /* Iterator it = c.iterator();
        while (it.hasNext()){
             System.out.println(it.next());
         }
         */
-       for (Iterator it = c.iterator();it.hasNext();){
-           System.out.println(it.next());
+        for (Iterator it = c.iterator (); it.hasNext (); ) {
+            System.out.println (it.next ());
 
-       }
+        }
 
 
     }
